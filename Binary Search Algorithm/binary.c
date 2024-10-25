e1a365e3f2fe0bc0cda6d7f8f915cdcd07a00337
Mostakin Ahmed <105800326+mostakinahmed@users.noreply.github.com>
2024-10-25 23:13:25 +0600

    Update binary.c

--- Binary Search Algorithm/binary.c
+++ Binary Search Algorithm/binary.c
@@ -7,6 +7,12 @@
     printf("H e l l o m World");
     printf("H e l l o");
     printf("H e l l o");
+      printf("H e l l o m World");
+      printf("H e l l o m World");
+      printf("H e l l o m World");
+      printf("H e l l o m World");
+      printf("H e l l o m World");
+      printf("H e l l o m World");
     return 0;
    
 }
